d151ea96fca426d3a0c2eeb94c2f343ecbbce987
Kartik Shah <97232854+KartikShah99@users.noreply.github.com>
2026-06-15 15:34:40 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def results():
 
 @app.route("/health", methods=["GET"])
 def health():
-    return jsonify({"status": "ok"})
+    return jsonify({"status": "veryok"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
